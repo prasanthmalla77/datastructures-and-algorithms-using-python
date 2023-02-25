c860ab70cba8aa73af5b175910d1a7d3f06d310c
MALLA NAGA VENKATA PRASANTH NAIDU <110344132+prasanthmalla77@users.noreply.github.com>
2023-02-26 01:18:54 +0530

    Add files via upload

---
+++ LISTS.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+LIST1=[1,2,3,"prasanth"]
+print(LIST1)
+
+
+# In[3]:
+
+
+#list Operations
+list2=["hello","prasanth","malla"]
+print("\nlist containing multiples values:",list2)
+
+
+# In[4]:
+
+
+list3=[["i","am"],["prasanth malla"]]
+print("\nmulti dimensional array:",list3)
+
+
+# In[6]:
+
+
+#accessing elements from list
+print("accessing elements from list:")
+print("\nlist element with index 0",list3[0])
+print("\nlist element with index 0",list3[1])
+
+
+# In[8]:
+
+
+#Accessing elements using negative indexing
+print("Accesing elements using negative indexing:")
+print("\nelement with index -1:",list3[-1])
+
+
+# In[ ]:
+
+
+
+
