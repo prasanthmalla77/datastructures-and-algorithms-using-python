f2f3d02c46803aa96648138a3d6b4418f8d1ee75
MALLA NAGA VENKATA PRASANTH NAIDU <110344132+prasanthmalla77@users.noreply.github.com>
2023-02-26 01:45:09 +0530

    Add files via upload

---
+++ STRINGS.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+string1="welcome to my college"
+print(string1[::-1])
+#this process is uused to return aall the elements back to front
+
+
+# In[4]:
+
+
+print(string1[0])
+
+
+# In[6]:
+
+
+print(string1.capitalize())#converts the first letter to capital
+
+
+# In[11]:
+
+
+print(string1.count("w"))#the number of times a specified value occurs in a string
+
+
+# In[13]:
+
+
+print(string1.split())#split the string and returns a list
+
+
+# In[ ]:
+
+
+
+
